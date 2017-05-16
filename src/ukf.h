6cfc2807fcd07bf91397a17ac9ded0748bfc8994
J Sistla <j.sistla@yahoo.com>
2017-05-15 19:56:28 -0500

    Initial working commit.

--- src/ukf.h
+++ src/ukf.h
@@ -84,30 +84,42 @@ public:
    */
   virtual ~UKF();
 
-  /**
-   * ProcessMeasurement
-   * @param meas_package The latest measurement data of either radar or laser
-   */
-  void ProcessMeasurement(MeasurementPackage meas_package);
+    /**
+     * Init Initializes Unscented Kalman filter
+     */
+    void Init();
 
-  /**
-   * Prediction Predicts sigma points, the state, and the state covariance
-   * matrix
-   * @param delta_t Time between k and k+1 in s
-   */
-  void Prediction(double delta_t);
+    /**
+     * Helper functions
+     */
+    MatrixXd AugmentedSigmaPoints();
 
-  /**
-   * Updates the state and the state covariance matrix using a laser measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateLidar(MeasurementPackage meas_package);
+    MatrixXd PredictedSigmaPoints(double timdediff);
 
-  /**
-   * Updates the state and the state covariance matrix using a radar measurement
-   * @param meas_package The measurement at k+1
-   */
-  void UpdateRadar(MeasurementPackage meas_package);
+    /**
+     * ProcessMeasurement
+     * @param meas_package The latest measurement data of either radar or laser
+     */
+    void ProcessMeasurement(MeasurementPackage meas_package);
+
+    /**
+     * Prediction Predicts sigma points, the state, and the state covariance
+     * matrix
+     * @param delta_t Time between k and k+1 in s
+     */
+    void Prediction(double delta_t);
+
+    /**
+     * Updates the state and the state covariance matrix using a laser measurement
+     * @param meas_package The measurement at k+1
+     */
+    void UpdateLidar(MeasurementPackage meas_package);
+
+    /**
+     * Updates the state and the state covariance matrix using a radar measurement
+     * @param meas_package The measurement at k+1
+     */
+    void UpdateRadar(MeasurementPackage meas_package);
 };
 
 #endif /* UKF_H */
